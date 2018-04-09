3fdb8239f92d7db32b3e790d883ed8128986be4b
Greg <ganders3@binghamton.edu>
2018-04-09 15:37:12 +0900

    edited package installer slightly

--- package_installer.R
+++ package_installer.R
@@ -1,4 +1,4 @@
-library(tools)
+# Installs all packages from their binary files, stored in a user-defined directory
 
 #===========================================================
 PACKAGE_DIR = 'directory/containing/package/zip/files'
@@ -7,7 +7,7 @@ PACKAGE_DIR = 'directory/containing/package/zip/files'
 installPackages = function(directory) {
     files = dir(directory)
     for (file in files) {
-        if (tolower(file_ext(file)) == 'zip') {
+        if (tolower(tools::file_ext(file)) == 'zip') {
             print(paste0('Attempting to install package from ', file, '...'))
             try(install.packages(paste0(PACKAGE_DIR, '/', file), repos = NULL, type = 'source'))
         }
